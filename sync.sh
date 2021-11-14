b6bcf8ccfe5e1a70053b7fa5ff9a27444f62eb2a
David Shen <dshen1@bu.edu>
2021-11-13 22:33:22 -0500

    Manage nvim config using nvim dir

---
+++ sync.sh
@@ -0,0 +1,5 @@
+dotfilespath='~/dotfiles/nvim'
+configpath='~/.config/nvim'
+echo "cp -r $configpath/after $dotfilespath"
+echo "cp -r $configpath/lua $dotfilespath"
+echo "cp $configpath/init.vim $dotfilespath"
