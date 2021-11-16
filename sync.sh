f8d5fcf4682686b4750fff3ea668068015d3542f
David Shen <dshen1@bu.edu>
2021-11-16 09:23:17 -0500

    Update sync script

--- sync.sh
+++ sync.sh
@@ -1,5 +1,11 @@
-dotfilespath='~/dotfiles/nvim'
-configpath='~/.config/nvim'
+dotfilespath="$HOME/me/dotfiles/nvim"
+configpath="$HOME/.config/nvim"
+
 echo "cp -r $configpath/after $dotfilespath"
+cp -r "$configpath/after" "$dotfilespath"
+
 echo "cp -r $configpath/lua $dotfilespath"
+cp -r "$configpath/lua" "$dotfilespath"
+
 echo "cp $configpath/init.vim $dotfilespath"
+cp "$configpath/init.vim" "$dotfilespath"
